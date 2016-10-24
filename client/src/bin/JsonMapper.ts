f134a1c8e6ec41b47289d6ed8e0f1b8e84a4626e
ytyszxf <ljz135790@gmail.com>
2016-10-24 16:01:05 +0800

    serializer deserializer

--- client/src/bin/JsonMapper.ts
+++ client/src/bin/JsonMapper.ts
@@ -1,24 +1,24 @@
 declare const Reflect: any;
-const jsonMetadataKey = "jsonProperty";
+const jsonMetadataKey = 'jsonProperty';
 
 export function Serializable() {
-  return function (clazz) { 
+  return function (clazz) {
     clazz.deserialize = (obj: any): any => {
       return MapUtils.deserialize(clazz, obj);
     };
 
-    clazz.prototype.serialize = function() {
+    clazz.prototype.serialize = function () {
       return MapUtils.serialize(this);
     };
-  }
+  };
 }
 
-export interface ISerializable{
-  serialize: ()=>any;
+export interface ISerializable {
+  serialize: () => any;
 }
 
-export function JsonProperty<T>(metadata?: IJsonMetaData<T> | string): any {
-  if (metadata instanceof String || typeof metadata === "string") {
+export function JsonProperty<T>(metadata: () => void | string | IJsonMetaData<T>): any {
+  if (metadata instanceof String || typeof metadata === 'string') {
     return Reflect.metadata(jsonMetadataKey, {
       name: metadata,
       clazz: undefined
@@ -27,7 +27,9 @@ export function JsonProperty<T>(metadata?: IJsonMetaData<T> | string): any {
     let metadataObj = <IJsonMetaData<T>>metadata;
     return Reflect.metadata(jsonMetadataKey, {
       name: metadataObj ? metadataObj.name : undefined,
-      clazz: metadataObj ? metadataObj.clazz : undefined
+      clazz: metadataObj ? metadataObj.clazz : undefined,
+      serilizer: metadataObj ? metadataObj.serializer : undefined,
+      deserilizer: metadataObj ? metadataObj.deserialize : undefined
     });
   }
 }
@@ -36,10 +38,12 @@ export function JsonProperty<T>(metadata?: IJsonMetaData<T> | string): any {
 export class MapUtils {
   static isPrimitive(obj) {
     switch (typeof obj) {
-      case "string":
-      case "number":
-      case "boolean":
+      case 'string':
+      case 'number':
+      case 'boolean':
         return true;
+      default:
+        break;
     }
     return !!(obj instanceof String || obj === String ||
       obj instanceof Number || obj === Number ||
@@ -47,7 +51,7 @@ export class MapUtils {
   }
 
   static getClazz(target: any, propertyKey: string): any {
-    return Reflect.getMetadata("design:type", target, propertyKey)
+    return Reflect.getMetadata('design:type', target, propertyKey);
   }
 
   static getJsonProperty<T>(target: any, propertyKey: string): IJsonMetaData<T> {
@@ -58,20 +62,29 @@ export class MapUtils {
     let clazz = obj.constructor;
     let result = {};
     Object.keys(obj).forEach((key) => {
-      let propertyMetadataFn: (IJsonMetaData) => any = (propertyMetadata) => {
+      let propertyMetadataFn: (d: IJsonMetaData<T>) => any = (propertyMetadata) => {
         let innerJson = undefined;
         innerJson = obj ? obj[key] : undefined;
         let clazz = MapUtils.getClazz(obj, key);
+        let metadata = MapUtils.getJsonProperty(obj, key);
         if (!MapUtils.isPrimitive(clazz)) {
           if (MapUtils.isArray(obj[key])) {
             return obj[key].map((item) => {
-              return MapUtils.serialize(item);
+              return metadata.serializer ?
+                metadata.serializer(item) :
+                MapUtils.serialize(item);
             });
           } else {
-            return MapUtils.serialize(obj[key]);
+            return metadata.serializer ?
+              metadata.serializer(obj[key]) :
+              MapUtils.serialize(obj[key]);
           }
         } else {
-          return obj ? obj[key] : undefined;
+          return obj ? (
+            metadata.serializer ?
+              metadata.serializer(obj[key]) :
+              obj[key]
+          ) : undefined;
         }
       };
 
@@ -92,7 +105,7 @@ export class MapUtils {
     if ((clazz === undefined) || (jsonObject === undefined)) return undefined;
     let obj = new clazz();
     Object.keys(obj).forEach((key) => {
-      let propertyMetadataFn: (IJsonMetaData) => any = (propertyMetadata) => {
+      let propertyMetadataFn: (d: IJsonMetaData<T>) => any = (propertyMetadata) => {
         let propertyName = propertyMetadata.name || key;
         let innerJson = jsonObject ? jsonObject[propertyName] : undefined;
         let clazz = MapUtils.getClazz(obj, key);
@@ -101,7 +114,9 @@ export class MapUtils {
           if (metadata.clazz || MapUtils.isPrimitive(clazz)) {
             if (innerJson && MapUtils.isArray(innerJson)) {
               return innerJson.map(
-                (item) => MapUtils.deserialize(metadata.clazz, item)
+                metadata.deserialize ?
+                  (item) => metadata.deserialize(item) :  
+                  (item) => MapUtils.deserialize(metadata.clazz, item)
               );
             } else {
               return undefined;
@@ -110,13 +125,23 @@ export class MapUtils {
             return innerJson;
           }
         } else if (!MapUtils.isPrimitive(clazz)) {
-          return MapUtils.deserialize(clazz, innerJson);
+          let metadata = MapUtils.getJsonProperty(obj, key);
+          return metadata.deserialize ?
+            metadata.deserialize(innerJson) :
+            MapUtils.deserialize(clazz, innerJson);
         } else {
-          return jsonObject ? jsonObject[propertyName] : undefined;
+          let metadata = MapUtils.getJsonProperty(obj, key);
+          return jsonObject ?
+            (
+              metadata.deserialize ?
+                metadata.deserialize(jsonObject[propertyName]) :
+                jsonObject[propertyName]
+            )
+            : undefined;
         }
       };
 
-      let propertyMetadata = MapUtils.getJsonProperty(obj, key);
+      let propertyMetadata = MapUtils.getJsonProperty<T>(obj, key);
       if (propertyMetadata) {
         obj[key] = propertyMetadataFn(propertyMetadata);
       } else {
@@ -131,17 +156,18 @@ export class MapUtils {
   static isArray(object) {
     if (object === Array) {
       return true;
-    } else if (typeof Array.isArray === "function") {
+    } else if (typeof Array.isArray === 'function') {
       return Array.isArray(object);
-    }
-    else {
+    } else {
       return !!(object instanceof Array);
     }
   }
 }
 
 export interface IJsonMetaData<T> {
-  name?: string,
-  clazz?: { new (): T }
+  name?: string;
+  clazz?: { new (): T };
+  serializer?: (t: T) => any;
+  deserialize?: (a: any) => T;
 }
 

4f045ff69331d8baa393354aa3554bb2f46f15b5
Zhanwen xu <leon.xu@kii.com>
2016-10-25 14:37:27 +0800

    architecture interface, developed #2

---
+++ server/api/services/RulesEngineService.js
@@ -0,0 +1,22 @@
+module.exports = {
+
+  create: function (options, done) {
+    return done()
+  },
+
+  update: function (options, done) {
+    return done()
+  },
+
+  delete: function (options, done) {
+    return done()
+  },
+
+  enable: function (options, done) {
+    return done()
+  },
+
+  disable: function (options, done) {
+    return done()
+  }
+};

062471e26ea811dc6480bf4b240166c029aa0ed2
Zhanwen xu <leon.xu@kii.com>
2016-10-25 14:37:27 +0800

    architecture interface, developed #2

---
+++ server/api/services/DataParserService.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+  toRulesEngine: function (options, done) {
+    return done()
+  },
+
+  toReporting: function (options, done) {
+    return done()
+  }
+
+  toDummy: function (options, done) {
+    return done()
+  }
+};
---
+++ server/api/services/UserAuthenticationService.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+  beehive: function (options, done) {
+    return done()
+  },
+
+  dummy: function (options, done) {
+    return done()
+  }
+};

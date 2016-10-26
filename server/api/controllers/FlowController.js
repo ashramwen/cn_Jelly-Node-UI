b4508946c6b8da7c6e549aa8fb6115e5ab4db329
Zhanwen xu <leon.xu@kii.com>
2016-10-26 10:39:58 +0800

    API UIs

---
+++ server/api/controllers/FlowController.js
@@ -0,0 +1,35 @@
+module.exports = {
+
+	save: function (req, res) {
+    sails.log.info(req.userID)
+		return res.ok()
+	},
+
+	create: function (req, res) {
+    
+  },
+
+  retrieve: function (req, res) {
+    
+  },
+
+  get: function (req, res) {
+    
+  },
+
+  update: function (req, res) {
+    
+  },
+
+  delete: function (req, res) {
+    
+  },
+
+  enable: function (req, res) {
+    
+  },
+
+  disable: function (req, res) {
+    
+  }
+};
--- server/api/policies/isGoodBody.js
+++ server/api/policies/isGoodBody.js
@@ -1,5 +1,9 @@
 module.exports = function(req, res, next) {
 
+	var contentType = req.headers['content-type']
+	if (!contentType || contentType != 'application/json')
+		return res.badRequest('Invalid Content Type')
+	
 	var body = req.body;
 
 	//TODO

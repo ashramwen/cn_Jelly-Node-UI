e49cc8bdf00a2d79050160e92105059c8bc5ea15
ytyszxf <ljz135790@gmail.com>
2016-11-30 10:09:11 +0800

    format

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -11,39 +11,39 @@ import { BeehiveThing } from './externals/resources/thing.type';
 import { JNFlow } from './core/models/jn-flow.type';
 
 import {
-  ApplicationContextService,
-  CacheContextService,
-  ConfigContextService
+ApplicationContextService,
+CacheContextService,
+ConfigContextService
 } from './core/services';
 
 @Component({
-    selector: 'app',
-    template: '<router-outlet></router-outlet>',
-    styles: [
-        require('jsoneditor/dist/jsoneditor.min.css'),
-        require('../assets/styles/theme.scss')
-    ],
-    encapsulation: ViewEncapsulation.None
+  selector: 'app',
+  template: '<router-outlet></router-outlet>',
+  styles: [
+    require('jsoneditor/dist/jsoneditor.min.css'),
+    require('../assets/styles/theme.scss')
+  ],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent implements OnInit {
 
-    constructor(private translate: TranslateService,
-        private application: JNApplication, private events: Events, private $thing: BeehiveThing) {
-        // this language will be used as a fallback when a translation isn't found in the current language
-        translate.setDefaultLang('cn');
+  constructor(private translate: TranslateService,
+    private application: JNApplication, private events: Events, private $thing: BeehiveThing) {
+    // this language will be used as a fallback when a translation isn't found in the current language
+    translate.setDefaultLang('cn');
 
-        // the lang to use, if the lang isn't available, it will use the current loader to get them
-        translate.use('cn');
+    // the lang to use, if the lang isn't available, it will use the current loader to get them
+    translate.use('cn');
 
-        translate.setTranslation('cn', cn);
-        translate.setTranslation('en', en);
-    }
+    translate.setTranslation('cn', cn);
+    translate.setTranslation('en', en);
+  }
 
-    ngOnInit() {
-        setTimeout(() => {
-            // let nodeFlow = new JNFlow();
-            // let node = nodeFlow.createNode(JNLocationNode);
-            // console.log(node);
-        }, 3000);
-    }
+  ngOnInit() {
+    setTimeout(() => {
+      // let nodeFlow = new JNFlow();
+      // let node = nodeFlow.createNode(JNLocationNode);
+      // console.log(node);
+    }, 3000);
+  }
 }

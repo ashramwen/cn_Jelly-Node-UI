1d28875e56f451aef712b7b6916200f0aa16432d
abattoiry <abattoiryue@163.com>
2016-12-12 15:27:18 +0800

    debug palette

--- client/src/app/externals/nodes/device-type-node/device-type-node-info-panel-model.type.ts
+++ client/src/app/externals/nodes/device-type-node/device-type-node-info-panel-model.type.ts
@@ -17,32 +17,4 @@ export class JNDeviceTypeNodeInfoPanelModel extends JNInfoPanelModel {
   generateComplexDataComponent() {
     return this.constructor;
   }
-
-
-  // protected createComplexDataHTML(complexData) {
-  //   let errorHTML = '<table>'
-  //   complexData.errors.forEach((error) => {
-  //     errorHTML = errorHTML + '<tr><td>'
-  //       + error.message +
-  //       '</td>' +
-  //       '</tr>';
-  //   })
-  //   errorHTML = errorHTML + '</table>';
-  //   let complexDataHTML = errorHTML;
-  //   return complexDataHTML;
-  // }
-
-  // protected createComplexDataCss() {
-  //   let tr = ' \
-  //   tr, td { border: 1px solid #999;\
-  //   padding: 0.5rem;\
-  //   text-align: left;\
-  //   font-size: 13px; } ';
-
-  //   let td = 'table{  border-collapse: collapse;\
-  //   margin: 10px auto;\
-  //   width: 100%;}';
-
-  //   return tr + td;
-  // }
 }
